08f5a4738467022ce7d2cb163b2f9143a59807a3
abisiaux <abisiaux>
2016-07-06 09:20:19 +0100

    a

--- providers/cpasbien.js
+++ providers/cpasbien.js
@@ -1,72 +1,70 @@
 var async   = require('async');
 var magnet  = require('magnet-uri');
 var util    = require('util');
+var parseTorrent = require('parse-torrent');
 
 var network = require('../network');
 
-var CPBAPI = require('cpasbien-api')
-var api = new CPBAPI()
-
-
-// ----------------------------------------------------------------------------
+const CPBAPI = require('cpasbien-api')
+const api = new CPBAPI()
 
 exports.movie = function(movieInfo, callback) {
-	api.Search(movieInfo.title, {scope: 'movies'})
-	  .then(function(err, data) {
-		  for (var i = 0; i < data.items.length; i++) {
-			  var magnetInfo = {
-			          title:  data.list[i].title,
-			          source: 'Cpasbien.io',
-			          size:   data.list[i].size,
-			          seeds:  data.list[i].seeds,
-			          peers:  data.list[i].leechs
-			        };
-			  magnetInfo.link = magnet.encode({
-		          dn: magnetInfo.title,
-		          xt: [ 'urn:btih:' + data.list[i].torrent ],
-		          tr: [
-		                'udp://tracker.internetwarriors.net:1337',
-		                'udp://tracker.coppersurfer.tk:6969',
-		                'udp://open.demonii.com:1337',
-		                'udp://tracker.leechers-paradise.org:6969',
-		                'udp://tracker.openbittorrent.com:80'
-		              ]
-		        });
+	api.Search(movieInfo.title).then((values) => {
+		var magnets = [];
+		var nb = 0;
+		for (var i = 0; i < values.items.length; i++) {
 
-		        magnets.push(magnetInfo);
-		  }
-		  
-		  callback(null, magnets);
-	  });
+			var magnetInfo = {
+				title:  values.items[i].title,
+				source: 'Cpasbien',
+				seeds:  values.items[i].seeds,
+				peers:  values.items[i].leechs
+			};
+
+			var size = values.items[i].size;
+			var split = size.split(" ");
+			var value = split[0].split(".");
+			if (split[1].startsWith("Ko")) {
+				magnetInfo.size = value[0] * 1024 + value[1];
+			} else if (split[1].startsWith("Mo")) {
+				magnetInfo.size = value[0] * 1024 * 1024 + value[1] * 1024;
+			} else if (split[1].startsWith("Go")) {
+				magnetInfo.size = value[0] * 1024 * 1024 *1024 + value[1] * 1024 * 1024;
+			}
+
+			parseTorrent.remote(values.items[i].torrent, function (err, parsedTorrent) {
+				nb = nb + 1;
+				if (err)  {
+					console.log(err);
+				} else {
+					console.log("Torrent OK : " + parsedTorrent.infoHash);
+					magnetInfo.link = magnet.encode({
+						dn: magnetInfo.title,
+						xt: [ 'urn:btih:' + parsedTorrent.infoHash ],
+						tr: [
+							'udp://tracker.internetwarriors.net:1337',
+							'udp://tracker.coppersurfer.tk:6969',
+							'udp://open.demonii.com:1337',
+							'udp://tracker.leechers-paradise.org:6969',
+							'udp://tracker.openbittorrent.com:80'
+						]
+					});
+	
+					magnets.push(magnetInfo);
+				}
+				
+				if (nb == values.items.length) {
+					console.log("Send callback");
+					callback(null, magnets);
+				}		
+			});
+		}
+	});
 }
 
-// ----------------------------------------------------------------------------
+
 
 exports.episode = function(showInfo, seasonIndex, episodeIndex, callback) {
-	callback(null, magnets);
-}
-
-// ----------------------------------------------------------------------------
-
-function mergeMagnetLists(list1, list2) {
-  var toAdd = [];
-
-  if (list2) {
-    for (var i = 0; i < list2.length; i++) {
-      var alreadyAdded = false;
-
-      for (var j = 0; j < list1.length; j++) {
-        if (list2[i].link == list1[j].link) {
-          alreadyAdded = true;
-          break;
-        }
-      }
-
-      if (!alreadyAdded) {
-        toAdd.push(list2[i]);
-      }
-    }
-  }
-
-  return list1.concat(toAdd);
+	 var magnets = [];
+	 callback(null, magnets);
 }
